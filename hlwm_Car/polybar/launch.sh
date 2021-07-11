85e152da4766709b9c453cfe7b8e7c90785b4248
jtstrader <11006561@live.mercer.edu>
2021-07-11 10:35:15 -0400

    formatting changes

--- hlwm_Car/polybar/launch.sh
+++ hlwm_Car/polybar/launch.sh
@@ -8,10 +8,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload top &
   done
 else
-  polybar --reload example &
+  polybar --reload top &
 fi
 
 echo "Bars launched..."
